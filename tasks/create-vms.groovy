fcdb995f11968b09893e8d6e82ecf19353a54205
root <root@li727-216.members.linode.com>
2019-04-23 05:08:49 +0000

    init

--- tasks/create-vms.groovy
+++ tasks/create-vms.groovy
@@ -12,9 +12,12 @@ node {
     ok = '\u2705'
     no = '\u274C'
 
+	subdir = dir('subDir')
+
     stage 'Preload check and run'
     node() {
         sh "sudo ls -ltrhR ${workspace}*"
+        sh "sudo ls -ltrhR ${subdir}"
         sh "sudo ls -ltrhR ./"
 		// sh "sudo sh ./lib/run.sh"
     }
