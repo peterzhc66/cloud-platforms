917305894e57fb1ade7ef1db4b6ef8b49740aeea
joehmchiu <joehmchiu@gmail.com>
2019-04-23 21:50:39 +1000

    Create start-vms.groovy

---
+++ tasks/start-vms.groovy
@@ -0,0 +1,27 @@
+node {
+    // loading and running jenkins tasks
+    workspace = pwd()
+
+    project = "cloud-platforms"
+    playbook= "start-vms.yml"
+    task = "gcp"
+    repositoryUrl = "https://github.com/joehmchiu/${project}.git"
+    branch = "master"
+    msg = "Remove VMs in GCP"
+
+    ok = '\u2705'
+    no = '\u274C'
+
+    stage 'Git Update'
+    node() {
+        git url: repositoryUrl, credentialsId: "gcp-2019", branch: branch
+        sh "ls -ltrhR"
+    }
+
+    stage 'Preload check and run'
+    node() {
+		sh "sudo sh ./lib/run.sh"
+    }
+
+	load "/opt/bin/jenkins-run.groovy"
+}
